68fbfb43d67228d6bc829883548bf612bef01989
Evgenii <lipatowevgeniy@gmail.com>
2019-09-21 16:19:16 +0600

    Change datetime field format in model

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -10,8 +10,8 @@ class Book(models.Model):
     author = models.CharField(max_length=50, null=False, blank=False, verbose_name='Имя автора')
     email = models.EmailField(max_length=70, null=False, blank=False, verbose_name="email автора")
     text = models.TextField(max_length=1500, null=False, blank=False, verbose_name='Текст записи')
-    CreatedDate = models.DateTimeField(auto_now=True)
-    LastEditDate = models.DateTimeField(auto_now_add=True)
+    CreatedDate = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+    LastEditDate = models.DateTimeField(auto_now=True, verbose_name="Дата редактирования")
     status = models.CharField(max_length=40, null=False, blank=False, verbose_name="Статус",
                               default=STATUS_CHOICES[0][0], choices=STATUS_CHOICES)
 
