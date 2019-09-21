7b2b7ebdb9bf754ff6b43416d37037c01d5473c9
Evgenii <lipatowevgeniy@gmail.com>
2019-09-21 13:48:32 +0600

    Second stage without design

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from webapp.models import Book, STATUS_CHOICES
 
-# Create your views here.
+
+def index_view(request, *args, **kwargs):
+    records = Book.objects.filter(status="active").order_by('-CreatedDate')
+    return render(request, 'index.html', context={
+        'records': records
+    })
